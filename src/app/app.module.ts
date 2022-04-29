b0487a9f27c1017e17e308bb805d72219b66c52c
karthik4901 <resukarthik@gmail.com>
2022-04-29 13:10:43 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { MaterialExampleModule } from './angular-material.module';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    SignUpComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialExampleModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class AppModule { }
